225d6c75a0b50e5d21fd0611853c8fc6ee211d5c
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-04-02 23:39:09 -0700

    Created reservation model and controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,70 @@
+class ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[ show edit update destroy ]
+
+  # GET /reservations or /reservations.json
+  def index
+    @reservations = Reservation.all
+  end
+
+  # GET /reservations/1 or /reservations/1.json
+  def show
+  end
+
+  # GET /reservations/new
+  def new
+    @reservation = Reservation.new
+  end
+
+  # GET /reservations/1/edit
+  def edit
+  end
+
+  # POST /reservations or /reservations.json
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    respond_to do |format|
+      if @reservation.save
+        format.html { redirect_to reservation_url(@reservation), notice: "Reservation was successfully created." }
+        format.json { render :show, status: :created, location: @reservation }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /reservations/1 or /reservations/1.json
+  def update
+    respond_to do |format|
+      if @reservation.update(reservation_params)
+        format.html { redirect_to reservation_url(@reservation), notice: "Reservation was successfully updated." }
+        format.json { render :show, status: :ok, location: @reservation }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /reservations/1 or /reservations/1.json
+  def destroy
+    @reservation.destroy
+
+    respond_to do |format|
+      format.html { redirect_to reservations_url, notice: "Reservation was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_reservation
+      @reservation = Reservation.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def reservation_params
+      params.require(:reservation).permit(:reservation_date, :appointment_id)
+    end
+end
