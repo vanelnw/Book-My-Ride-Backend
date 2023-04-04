0156766460635267e8262f5efe2f382abfdb9260
Abeera Tahir <abeeratahir2018@gmail.com>
2023-04-05 01:31:08 +0500

    Install packages and setup cors

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.2.0'
 
+gem 'bcrypt', '~> 3.1.7'
+gem 'jwt', '~> 2.2.3'
+gem 'rack-cors'
+
 gem 'rubocop', '>= 1.0', '< 2.0'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module AppointmentCapstoneBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', 
+          headers: :any, 
+          methods: [:get, :post, :put, :patch, :delete, :options, :head], 
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'], 
+          max_age: 600
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
