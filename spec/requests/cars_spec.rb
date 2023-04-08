9e8fb9ab7e3a0395f2aaac7aba31c7ecb020b6fd
Abeera Tahir <abeeratahir2018@gmail.com>
2023-04-08 23:46:42 +0500

    Add spec for reservations create action

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 describe "GET /api/v1/cars/:id" do
   it "renders a JSON response with the car" do
-    SECRET_KEY = Rails.application.secret_key_base
-    @user = User.create(name: "user1", email: "user1@example.com", password_digest: "password")
-    payload = { user_id: @user.id }
-    token = JWT.encode(payload, SECRET_KEY)
+    user = User.create(name: "user1", email: "user1@example.com", password_digest: "password")
+    payload = { user_id: user.id }
+    token = JWT.encode(payload, Rails.application.secret_key_base)
     headers = { "Authorization" => "Bearer #{token}" }
-    @car = Car.create(make: "Ford", model: "Mustang", year: 2020, price: 40000, user_id: @user.id, image: "https://imgd.aeplcdn.com/0x0/cw/ec/23766/Ford-Mustang-Exterior-126883.jpg?wm=0")
+    car = Car.create(make: "Ford", model: "Mustang", year: 2020, price: 40000, user_id: user.id, image: "https://imgd.aeplcdn.com/0x0/cw/ec/23766/Ford-Mustang-Exterior-126883.jpg?wm=0")
 
-    get "/api/v1/cars/#{@car.id}", headers: headers
+    get "/api/v1/cars/#{car.id}", headers: headers
 
     expect(response).to have_http_status(:ok)
     expect(response.content_type).to eq("application/json; charset=utf-8")
-    expect(response.body).to include(@car.to_json)
+    expect(response.body).to include(car.to_json)
   end
 end
