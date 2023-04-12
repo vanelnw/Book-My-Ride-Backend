37ef0c213037e274e5a7fa2bf4b7704f29ddf622
Abeera Tahir <abeeratahir2018@gmail.com>
2023-04-12 07:03:58 +0500

    Add login spec

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::AuthenticationController, type: :request do
-  describe 'POST /api/v1/auth/register' do
-    let(:valid_params) { { name: 'Test User', email: 'test@example.com', password: 'password' } }
+  describe 'POST authentication' do
+    let(:signup_params) { { name: 'Test User', email: 'test@example.com', password: 'password' } }
+    let(:login_params) { { email: 'test@example.com', password: signup_params[:password] } }
 
     before do
-      post '/api/v1/auth/register', params: valid_params.to_json, headers: { 'Content-Type' => 'application/json' }
+      post '/api/v1/auth/register', params: signup_params.to_json, headers: { 'Content-Type' => 'application/json' }
     end
 
     it 'creates a user' do
@@ -21,6 +22,12 @@ RSpec.describe Api::V1::AuthenticationController, type: :request do
 
     it 'returns a status code 201' do
       expect(response).to have_http_status(:created)
+    end
+
+    it 'logins the user' do
+      post '/api/v1/auth/login', params: login_params.to_json, headers: { 'Content-Type' => 'application/json' }
+      json_response = JSON.parse(response.body)
+      expect(json_response['message']).to eq('Logged in successfully!')
     end
   end
 end
