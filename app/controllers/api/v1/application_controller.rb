2072cf29c368cdb87ab6edca6a6da4971427a8d5
Vanel <vanelnwaba@gmail.com>
2023-04-14 15:07:05 +0100

    update jsonwebtoken2

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::ApplicationController < ActionController::API
   def authenticate_request
     header = request.headers['Authorization']
     header = header.split.last if header
-    decoded = JsonWebToken.jwt_decode(header)
+    decoded = Api::V1::JsonWebToken.jwt_decode(header)
     @current_user = User.find(decoded[:user_id])
   end
 end
